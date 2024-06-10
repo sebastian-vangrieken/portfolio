f0a5e2cb082960fc797c6d350031253ac3503c38
sebastian-vangrieken <r0889358@student.thomasmore.be>
2024-06-10 16:36:05 +0200

    skills update

--- src/components/SkillSlider.tsx
+++ src/components/SkillSlider.tsx
@@ -13,9 +13,9 @@ function SkillSlider({ skill, proficiency, darkMode }: SkillSliderProps) {
     return (
         <div className="w-fit my-4">
             <svg height={26} width={306}>
-                <text x="0" y="12" fontSize={16} fill={text}>{skill}</text>
-                <line x1="0" y1="20" x2="300" y2="20" style={{ strokeWidth:'8', strokeLinecap:'round' }} stroke="#EEE" />
-                <line x1="0" y1="20" x2={3 * proficiency} y2="20" style={{ stroke:color, strokeWidth:'8', strokeLinecap:'round' }} />
+                <text x="5" y="12" fontSize={16} fill={text}>{skill}</text>
+                <line x1="5" y1="20" x2="300" y2="20" strokeLinecap="round" strokeWidth="8" stroke="#EEE" />
+                <line x1="5" y1="20" x2={3 * proficiency} y2="20" style={{ stroke:color, strokeWidth:'8', strokeLinecap:'round' }} />
                 <circle r="6" cx={3 * proficiency} cy="20" style={{ fill:color }} />
                 <text x={width} y="10" fontSize={fontSize} fill={text}>{proficiency}%</text>
             </svg>     
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -9,19 +9,52 @@ function Skills({ darkMode } : SkillsProps) {
     return (
         <div id="skills" className="pb-44">
             <h1 className="text-3xl font-bold border-b-4 w-fit mb-4 border-highlightl dark:border-highlightd">Skills</h1>
-            <p className="mb-4">Hieronder zie je een paar van de technologieën waar ik de afgelopen 3 jaar veel en graag met heb gewerkt.</p>
+            <p className="flex-wrap w-3/5 mb-4">In de afgelopen 3 jaar heb ik natuurlijk al met veel verschillende technologieen gewerkt. 
+                Hieronder vind je alvast een paar van mijn meest gebruikte of favoriete.</p>
             <div className="flex justify-around">
                 <div>
-                    <SkillSlider skill="Android development" proficiency={80} darkMode={darkMode} />
-                    <SkillSlider skill="Java" proficiency={75} darkMode={darkMode} />
-                    <SkillSlider skill=".NET" proficiency={70} darkMode={darkMode} />
-                    <SkillSlider skill="html&css" proficiency={75} darkMode={darkMode} />
+                    <div className="pb-4">
+                        <p className="flex items-center">
+                            <img src="./android-svgrepo-com.svg" alt="" className="h-12 w-12" />
+                            <SkillSlider skill="Android development" proficiency={80} darkMode={darkMode} />
+                        </p>
+                        <p className="flex items-center">
+                            <img src="./unreal-engine-svgrepo-com.svg" alt="" className="h-8 w-8 me-4" />
+                            <SkillSlider skill="Unreal engine" proficiency={55} darkMode={darkMode} />
+                        </p>
+                    </div>
+                    <div>
+                        <p className="flex items-center">
+                            <img src="./html-5-svgrepo-com.svg" alt="" className="h-10 w-10 me-2" />
+                            <SkillSlider skill="html&css" proficiency={75} darkMode={darkMode} />
+                        </p>
+                        <p className="flex items-center">
+                            <img src="./typescript-svgrepo-com.svg" alt="" className="h-8 w-8 me-4" />
+                            <SkillSlider skill="Typscript" proficiency={70} darkMode={darkMode} />
+                        </p>
+                    </div>
                 </div>
                 <div>
-                    <SkillSlider skill="Typscript" proficiency={70} darkMode={darkMode} />
-                    <SkillSlider skill="Angular" proficiency={70} darkMode={darkMode} />
-                    <SkillSlider skill="React" proficiency={65} darkMode={darkMode} />
-                    <SkillSlider skill="Docker" proficiency={60} darkMode={darkMode} />
+                    <div className="pb-4">
+                        <p className="flex items-center"> 
+                            <img src="./angular-svgrepo-com.svg" alt="" className="h-8 w-8 me-2" />
+                            <SkillSlider skill="Angular" proficiency={70} darkMode={darkMode} />
+                        </p>
+                        <p className="flex items-center">
+                            <img src="./react-svgrepo-com.svg" alt="" className="h-8 w-8 me-2" />
+                            <SkillSlider skill="React" proficiency={65} darkMode={darkMode} />
+                        </p>
+                    </div>
+                    <div>
+                        <p className="flex items-center">
+                            <img src="./java-svgrepo-com.svg" alt="" className="h-8 w-8 me-2" />
+                            <SkillSlider skill="Java" proficiency={75} darkMode={darkMode} />
+                        </p>
+                        <p className="flex items-center">
+                            <img src="./csharp-svgrepo-com.svg" alt="" className="h-8 w-8 me-2" />
+                            <SkillSlider skill=".NET" proficiency={70} darkMode={darkMode} />
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
